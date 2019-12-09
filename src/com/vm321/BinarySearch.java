74786ec026c9a23e49eaf1474b5ca2d2da52fed1
megatron <megatron@megatrondeiMac.local>
2019-12-09 20:19:40 +0800

    adjust

--- src/com/vm321/BinarySearch.java
+++ src/com/vm321/BinarySearch.java
@@ -1,4 +1,38 @@
 package com.vm321;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 12, 87, 66, 123, 874};
+        Arrays.sort(arr);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入key: ");
+        int key = scan.nextInt();
+        int index=rank(key, arr);
+        if(index < 0){
+            System.out.println("无法匹配");
+        }else{
+            System.out.println("属于第" + index + "位");
+        }
+    }
+
+    private static int rank(int key, int[] arr) {
+        int lo = 0;
+        int hi = arr.length -1;
+        int i = 0;
+        while(lo <= hi){
+            i++;
+            int mid = lo + (hi - lo) / 2;
+            if(key < arr[mid]){
+                hi = mid - 1;
+            }else if (key > arr[mid]){
+                lo = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
